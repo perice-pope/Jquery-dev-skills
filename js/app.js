7c5c2752beff7b7486ac127aa48744b26f145d8d
Perice Pope <perice09@gmail.com>
2019-09-06 10:59:27 -0700

    need to add delete button

--- js/app.js
+++ js/app.js
@@ -8,19 +8,21 @@
 
 $('.button').click(function(e){
     e.preventDefault()
-    $('.future-skills').append($('<p>', { text: $('input').val() })
+    $('.future-skills').append($(
+        '<p>', 
+        { text: $('input').val() },
+        "<li><button class='remove'>X</button>"
+        )
 
     );
 });
-
-{/* <li><button class="remove">X</button> */}
 
 
 
 //     <td><button class="remove">X</button> ${newSkill}</td>
 //     </tr>
-//     `
-//     $(input).appendTo($("li"));
+
+
 //     $('.button').on('click', removeSkill)
 //     });
 //     function removeSkill(evt){
