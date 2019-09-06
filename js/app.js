87a6e539859a095bd54f1bba3196dee95a5ad337
Perice Pope <perice09@gmail.com>
2019-09-06 16:25:09 -0700

    delete and add functions added

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 
 $('.button').click(function(e){
+    
     e.preventDefault()
-    $('.future-skills').append($(
-        '<p>', 
-        { text: $('input').val() }, 
-        
-        )
-    ) 
+    let result = $('input').val()
+    $('.future-skills').append($(`<p><button id=del>x</button>${result}</p>`)) 
+  result = $('input').val(" ")
 });
 
-$('.button').on('click', '#remove', function(f) {
-    f.preventDefault()
-    $(this).closest('p').remove(); 
-});
 
-    function remove(evt){
-       $(evt.target).parent().remove()
-       console.log('event-killed-by-destroyer')
-    }
+$('.future-skills').on('click', ' button ', function(e) {
+    e.preventDefault()
+    // console.dir('the target') <---- to look up the target elements in the DOM
+    // console.log(e.target.parentElement)
+    e.target.parentElement.remove()
+})
+
+    // function remove(evt){
+    //    $(evt.target).parent().remove()
+    //    console.log('event-killed-by-destroyer')
+    // }
